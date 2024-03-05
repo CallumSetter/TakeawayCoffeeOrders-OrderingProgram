ce6dc1555982f94fa91960a1c844a78adecc7ad4
Callum Setter <calsett@chbc.school.nz>
2024-03-05 13:12:53 +1300

    <Created a menu>

---
+++ TakeawayCoffeeOrders-OrderingProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿//Imports
+using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TakeawayCoffeeOrders_OrderingProgram
+{
+    class Program
+    {
+        //Global Variables
+        static List<string> waitingList = new List<string>();
+
+        static void Main()
+        {
+            //A takeaway coffee is demanded by the user
+
+            Console.WriteLine("Hello what type of coffee would you like to order\n");
+            Console.WriteLine("---------- Menu ----------");
+            Console.WriteLine("For a Flat White Press 1\n" +
+                "For a Latte Press 2\n" +
+                "For a Chai Latte Press 3\n" +
+                "For a Short Black Press 4\n" +
+                "For a Long Black Press 5\n" +
+                "For a Cappuccino Press 6\n" +
+                "For a Mocha Press 7\n" +
+                "--------------------------");
+
+            //Make sure the user can only order a takeaway coffee
+
+          
+        }
+
+      
+
+    }
+}
+
+
