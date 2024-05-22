949994127fe93aeeebd8d62d9cfc65b8bde26b36
Callum Setter <calsett@chbc.school.nz>
2024-05-22 14:43:00 +1200

    <Added Console.Clear(); after infomation on each coffee type so the user won't be over whelmed with all the information.>

--- TakeawayCoffeeOrders-OrderingProgram/Program.cs
+++ TakeawayCoffeeOrders-OrderingProgram/Program.cs
@@ -37,7 +37,7 @@ namespace TakeawayCoffeeOrders_OrderingProgram
                     "----------------------\n" +
                     "More - Press 9\n");
 
-            
+           
             while (true)
             {
 
@@ -146,35 +146,64 @@ namespace TakeawayCoffeeOrders_OrderingProgram
                             "\n------------------------------" +
                             "\nCancel - Press 5\n");
 
-                        
+                       
 
 
 
                         break;
+
+                        //Give the user an option to view information about each type of coffee
                     case "9":
                         Console.WriteLine(
-                            "\nFlat White:\n" +
-                        "An espresso-based drink that contains steamed milk. Flat Whites originated in New Zealand in the 1980s and several years later, Flat Whites began to appear on menus in countries such as the United States and the United Kingdom. Flat Whites are often mistaken for Lattes but what separates them is that Flat Whites have a stronger coffee taste, giving Lattes a sweeter taste.\n" +
-                            
-                            "\nLatte:\n" +
-                            "A milk coffee containing a silky layer of foam. Lattes are usually made up of one or two shots of espresso, steamed milk and topped with a thin layer of frothed milk. The first origins of Lattes are not very clear as people have been combining coffee and milk for centuries. Modern Lattes that people know today originated in Seattle, United States during the 1980s.\n" +
-                        "\nChai Latte:\n" +
-                        "A hot, milky, fragrant, gently spicy type of coffee. What makes Chai Lattes different from other coffees is that doesn't contain any coffee. Chai Lattes contain a sweetened chai syrup or powder allowing them to be made quickly. Chai Lattes originated in India thousands of years ago and have spread in popularity over the last two centuries.\n" +
-                        "\nShort Black:\n" +
-                        "A single espresso shot without milk. Short Blacks are great to have when you just want to enjoy the pure taste of coffee. Short Blacks have a very strong taste as they only contain an espresso shot and hot water. It isn’t too clear where short black coffees originate from, however they are globally popular appearing on most Cafe menus. \n" +
-                        "\nLong Black:\n" +
-                        "An espresso-based drink popular in Australia and New Zealand. The only ingredients used in a long black is espresso and hot water. Long Blacks are traditionally made by pouring a double shot of espresso or ristretto over 100 to 120ml of hot water from the espresso machine. Long Blacks have always been confused with Americanos, but their main difference is how the hot water is mixed with the espresso. An Americano is made by pouring hot water over espresso while a Long Black is made in the opposite way.  \n" +
-                        "\nCappuccino:\n" +
-                        "A perfect balance of espresso steamed milk and foam. Cappuccinos are all about the structure of splitting the three elements into equal thirds. Cappuccinos are supposed to be rich but not acidic, also they should have a mildly sweet flavouring from the milk. The milk is not actually mixed in with the Cappuccino giving it a stronger flavour. Cappuccinos originated in Italy and then made their way across the world we’re they are now seen on most cafe menus.  \n" +
-                        "\nMocha:\n" +
-                        "A variant of a latte being a deliciously sweet, nutty and chocolatey flavoured coffee. Mochas is an espresso-based drink; however, it is different from most other coffees due to its sweeter taste. Mochas are made with a shot of espresso, followed by milk and cream. It can also be combined with chocolate powder or syrup.  The term ‘mocha’ is a specific type of coffee bean which is only grown in the city of Mocha, Yemen. \n" +
-                        "\nAmericano:\n" +
-                        "It is simply just hot water and espresso. They are made by adding the espresso first, then the crema is added after to give a mellow taste. Americanos originate back to World War II when American soldiers were stationed in Italy, the American soldiers didn’t like the strong espresso that was popular in Italy, so they tried to recreate their beloved drip coffee from America by adding water to an espresso shot. This is now known today is an Americano. \n");
+                            "\n1. Flat White:\n" +
+                        "An espresso-based drink that contains steamed milk. Flat Whites originated in New Zealand in the 1980s and several years later, Flat Whites began to appear on menus in countries such as the United States and the United Kingdom. Flat Whites are often mistaken for Lattes but what separates them is that Flat Whites have a stronger coffee taste, giving Lattes a sweeter taste.\n");
+                        Console.WriteLine("Press any key to view information about Lattes");
+                        Console.ReadLine();
+                            Console.Clear();
 
+                        Console.WriteLine("\n2. Latte:\n" +
+                            "A milk coffee that contains a silky layer of foam. Lattes are usually made up of one or two shots of espresso, steamed milk and topped with a thin layer of frothed milk. The first origins of Lattes are not very clear as people have been combining coffee and milk for centuries. Modern Lattes that people know today originated in Seattle, United States during the 1980s. \n");
+                        Console.WriteLine("Press any key to view information about Chai Lattes");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("\n3. Chai Latte:\n" +
+                            "A hot, milky, fragrant, gently spicy type of coffee. What makes Chai Lattes different from other coffees is that doesn't contain any coffee. Chai Lattes contain a sweetened chai syrup or powder allowing them to be made quickly. Chai Lattes originated in India thousands of years ago and have spread in popularity over the last two centuries.\n");
+                        Console.WriteLine("Press any key to view information about Short Blacks");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("\n4. Short Black:\n" +
+                            "A single espresso shot without milk. Short Blacks are great to have when you just want to enjoy the pure taste of coffee. Short Blacks have a very strong taste as they only contain an espresso shot and hot water. It isn’t too clear where short black coffees originate from, however they are globally popular appearing on most Cafe menus. \n");
+                        Console.WriteLine("Press any key to view information about Long Blacks");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("\n5. Long Black:\n" +
+                            "An espresso-based drink popular in Australia and New Zealand. The only ingredients used in a long black is espresso and hot water. Long Blacks are traditionally made by pouring a double shot of espresso or ristretto over 100 to 120ml of hot water from the espresso machine. Long Blacks have always been confused with Americanos, but their main difference is how the hot water is mixed with the espresso. An Americano is made by pouring hot water over espresso while a Long Black is made in the opposite way.  \n");
+                        Console.WriteLine("Press any key to view information about Cuppuccinos");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("\n6. Cappuccino:\n" +
+                            "A perfect balance of espresso steamed milk and foam. Cappuccinos are all about the structure of splitting the three elements into equal thirds. Cappuccinos are supposed to be rich but not acidic, also they should have a mildly sweet flavouring from the milk. The milk is not actually mixed in with the Cappuccino giving it a stronger flavour. Cappuccinos originated in Italy and then made their way across the world we’re they are now seen on most cafe menus.  \n");
+                        Console.WriteLine("Press any key to view information about Mochas");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("\n7. Mocha:\n" +
+                            "A variant of a latte being a deliciously sweet, nutty and chocolatey flavoured coffee. Mochas is an espresso-based drink; however, it is different from most other coffees due to its sweeter taste. Mochas are made with a shot of espresso, followed by milk and cream. It can also be combined with chocolate powder or syrup.  The term ‘mocha’ is a specific type of coffee bean which is only grown in the city of Mocha, Yemen. \n");
+                        Console.WriteLine("Press any key to view information about Americanos");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("\n8. Americano:\n" +
+                            "Simply just hot water and espresso. They are made by adding the espresso first, then the crema is added after to give a mellow taste. Americanos originate back to World War II when American soldiers were stationed in Italy, the American soldiers didn’t like the strong espresso that was popular in Italy, so they tried to recreate their beloved drip coffee from America by adding water to an espresso shot. This is now known today is an Americano. \n");
+                        Console.WriteLine("Press any key to return to the menu");
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
-                    default:
-                        Console.WriteLine("Sorry, that is not an option");
-                        break;
+                    
 
 
                        
@@ -199,20 +228,20 @@ namespace TakeawayCoffeeOrders_OrderingProgram
 
 
 
-                Thread.Sleep(900);
+                Thread.Sleep(1000);
                 Console.WriteLine("Measuring the brew ratio");
 
 
-                Thread.Sleep(900);
+                Thread.Sleep(1000);
                 Console.WriteLine("Grinding the coffee beans");
 
-                Thread.Sleep(900);
+                Thread.Sleep(1000);
                 Console.WriteLine("Boiling the water");
 
-                Thread.Sleep(900);
+                Thread.Sleep(1000);
                 Console.WriteLine("Placing coffee into the filter");
 
-                Thread.Sleep(900);
+                Thread.Sleep(1000);
                 Console.WriteLine("Pouring the coffee\n");
 
                 Console.WriteLine("Thank you for your order!");
@@ -221,26 +250,29 @@ namespace TakeawayCoffeeOrders_OrderingProgram
 
 
 
+                
                 while (true)
                 {
                     Console.WriteLine("Would you like to run the program again\n" +
-                        "Run Again  - Press 1\n" +
-                        "Exit       - Press 2 ");
-                        
+                       "Run Again  - Press 1\n" +
+                       "Exit       - Press 2 ");
 
                     string proceed = Console.ReadLine();
                     if (!string.IsNullOrWhiteSpace(proceed))
                     {
                         proceed = proceed.ToLower().Substring(0, 1);
 
-                        if (proceed.Equals("1") || proceed.Equals("2"))
+                        if (proceed.Equals("1") || proceed.Equals("2")) 
                         {
                             return proceed;
                         }
                     }
                     Console.WriteLine("Please enter either options '1' or '2'\n\n");
-
                 }
+                   
+                        
+
+                   
 
 
 
